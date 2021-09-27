34a24975298459e3e7064c0d5176b6611996485a
Rohan Patel <78699164+rohanbhadreshpatel@users.noreply.github.com>
2021-09-27 00:12:46 -0500

    Update Delete.js

--- UI/my-twitter-app-ui/src/Delete.js
+++ UI/my-twitter-app-ui/src/Delete.js
@@ -2,7 +2,7 @@ export default class Delete{
     static DeleteTweet(status) {
       console.log(status)  
       return fetch('http://127.0.0.1:5000/tweet/delete/'+status.status,{
-        'method':'DELETE',
+        'method':'POST',
         headers : {
           'Content-Type':'application/json'
         }})
